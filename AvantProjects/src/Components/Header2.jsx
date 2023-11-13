0bf173aeaed1ee1b5204533364ba27b12aafe383
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-11-13 18:10:23 +0300

    Moved, Renamed and Created componets.

---
+++ AvantProjects/src/Components/Header2.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header2.css';
+
+function Header2() {
+  return (
+    <div className='header2'>
+        <div className='company-logo'></div>
+        <div className='main-nav'></div>
+        <div className='get_a_quote-button'></div>
+    </div>
+  )
+}
+
+export default Header2
---
+++ AvantProjects/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import Header1 from '../Components/Header1';
+
+function Home() {
+  
+
+  return (
+    <>
+      <Header1 />
+    </>
+  )
+}
+
+export default Home
