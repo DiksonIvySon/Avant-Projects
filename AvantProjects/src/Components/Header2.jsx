35dfc1fcbd84433b1080c05aa85a95f69ee94db1
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-11-13 22:20:35 +0300

    Worked on Header2: Not Done with it.

--- AvantProjects/src/Components/Header2.jsx
+++ AvantProjects/src/Components/Header2.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header2.css';
+import logo from '../images/logo.jpeg'
 
 function Header2() {
+  const [toggleHam_icon, setToggleHam_icon] = useState("close");
+
+  const HandleToggleHam = () => {
+    if (toggleHam_icon === "close") {
+        setToggleHam_icon("open");
+    }
+    else {
+        setToggleHam_icon("close");
+    }
+  }
+
   return (
     <div className='header2'>
-        <div className='company-logo'></div>
-        <div className='main-nav'></div>
-        <div className='get_a_quote-button'></div>
+        <div className='company-logo'>
+            <img src={logo} alt="logo image" />
+        </div>
+        <div id='main-nav' className='responsive'>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+        <div className='get_a_quote-button'>
+            <a href="">
+                <button>Get A Quote</button>
+            </a>
+        </div>
+        <div id="nav-icon" className={toggleHam_icon} onClick={HandleToggleHam}>
+            <span></span>
+            <span></span>
+            <span></span>
+         </div>
     </div>
   )
 }
--- AvantProjects/src/pages/Home.jsx
+++ AvantProjects/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Header1 from '../Components/Header1';
+import Header2 from '../Components/Header2';
 
 function Home() {
   
@@ -7,6 +8,7 @@ function Home() {
   return (
     <>
       <Header1 />
+      <Header2 />
     </>
   )
 }
