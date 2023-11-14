7b9cbf9bd30997ab5eef73fe4ee656f427df6eef
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-11-14 17:11:42 +0300

    Done with Header2 functionality and animation

--- AvantProjects/src/Components/Header2.jsx
+++ AvantProjects/src/Components/Header2.jsx
@@ -4,22 +4,28 @@ import logo from '../images/logo.jpeg'
 
 function Header2() {
   const [toggleHam_icon, setToggleHam_icon] = useState("close");
+  const [verticalMenu, setVerticalMenu] = useState("hide");
+  const [logoState, setLogoState] = useState("company-logo");
 
   const HandleToggleHam = () => {
     if (toggleHam_icon === "close") {
         setToggleHam_icon("open");
+        setVerticalMenu("")
+        setLogoState("company-logo-hide")
     }
     else {
         setToggleHam_icon("close");
+        setVerticalMenu("hide")
+        setLogoState("company-logo")
     }
   }
 
   return (
     <div className='header2'>
-        <div className='company-logo'>
+        <div className={logoState}>
             <img src={logo} alt="logo image" />
         </div>
-        <div id='main-nav' className='responsive'>
+        <div id="main-nav" className={verticalMenu} >
             <ul>
                 <li><a href="">Home</a></li>
                 <li><a href="">About</a></li>
@@ -27,11 +33,11 @@ function Header2() {
                 <li><a href="">Projects</a></li>
                 <li><a href="">Contact</a></li>
             </ul>
-        </div>
-        <div className='get_a_quote-button'>
-            <a href="">
-                <button>Get A Quote</button>
-            </a>
+            <div className='get_a_quote-button'>
+                    <a href="">
+                        <button>Get A Quote</button>
+                    </a>
+            </div>
         </div>
         <div id="nav-icon" className={toggleHam_icon} onClick={HandleToggleHam}>
             <span></span>
