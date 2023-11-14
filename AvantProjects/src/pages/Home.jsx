af1f64f9e3a0d138f1f299cdba21e7e88ed4cfd4
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-11-14 20:54:07 +0300

    Created the slogan container and styled it.

--- AvantProjects/src/pages/Home.jsx
+++ AvantProjects/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import './Home.css';
 import Header1 from '../Components/Header1';
 import Header2 from '../Components/Header2';
 import Banner from '../Components/Banner';
@@ -8,9 +9,24 @@ function Home() {
 
   return (
     <>
+    <div className='firstPage'>
       <Header1 />
       <Header2 />
       <Banner />
+      <div className='slogan-container'>
+        <div className='sloganBox'>
+          <h1 className='slogan1'>Building The Future</h1>
+          <h1 className='slogan2'>Restoring The Past</h1>
+          <p className='slogan-paragraph'>
+            Our guiding principles are based on the goal of continuously exceeding our clients' expectations 
+            via dependable response times, evident dedication, and genuine interest in the construction industry.
+          </p>
+          <a href="">
+            <button className='contact-button'>Contact Us</button>
+          </a>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
