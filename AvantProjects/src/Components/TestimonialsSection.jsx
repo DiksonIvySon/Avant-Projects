26201c40dc0eb1abbab9298dd7501a74f20ddb72
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-11-21 18:20:46 +0300

    Made logo and text changes in the tesimonials section.

--- AvantProjects/src/Components/TestimonialsSection.jsx
+++ AvantProjects/src/Components/TestimonialsSection.jsx
@@ -23,12 +23,12 @@ function TestimonialsSection() {
             <div className='testimonial'>
                 <div className='testimonial-header'>
                     <div className='company-logo'>
-                    <img src={abe} alt="Company Logo" />
+                        <img src={abe} alt="Company Logo" />
                     </div>
                     <div className='testimonial-writer'>
-                    <h4>Bruce R Smith</h4>
-                    <p>Director</p>
-                    <p>Project Management</p>
+                        <h4>Bruce R Smith</h4>
+                        <p>Director</p>
+                        <p>Project Management</p>
                     </div>
                 </div>
                 <hr />
@@ -44,19 +44,19 @@ function TestimonialsSection() {
             <div className='testimonial'>
                 <div className='testimonial-header'>
                     <div className='company-logo'>
-                    <img src={abe} alt="Company Logo" />
+                        <img src={abe} alt="Company Logo" />
                     </div>
                     <div className='testimonial-writer'>
-                    <h4>Bruce R Smith</h4>
-                    <p>Director</p>
-                    <p>Project Management</p>
+                        <h4>Eric Gouws (Pty) Ltd</h4>
+                        <p>abe</p>
+                        <p>Construction Chemicals</p>
                     </div>
                 </div>
                 <hr />
                 <p className='testimonial-paragraph'>
-                        <span>"</span>Having worked for and with Avant projects in various roles since 1990 , I have been very satisfied with their focus 
-                        and drive applicable to the support and cooperation I had in all my roles in the company. They deal with any issues 
-                        that need additional attention and keep their customers up to date with progress. I recommend their reliability... <span>"</span>
+                        <span>"</span>This is to confirm that Avant has been in the construction industry for 24+ years and have been members of the GMBA 
+                        for more than 20 years. abe Construction Chemicals (Pty) Limited has no hesitation in recommending them and granting them APPROVED 
+                        APPLICATOR / CONTRACTOR STATUS... <span>"</span>
                 </p>
                 <a href="">
                     <button className='button1'>Open Original Testimonial</button>
@@ -65,19 +65,19 @@ function TestimonialsSection() {
             <div className='testimonial'>
                 <div className='testimonial-header'>
                     <div className='company-logo'>
-                    <img src={abe} alt="Company Logo" />
+                        <img src={pronto} alt="Company Logo" />
                     </div>
                     <div className='testimonial-writer'>
-                    <h4>Bruce R Smith</h4>
-                    <p>Director</p>
-                    <p>Project Management</p>
+                        <h4>MARISCA HABIB</h4>
+                        <p>Sales / Admin</p>
+                        <p>Manager</p>
                     </div>
                 </div>
                 <hr />
                 <p className='testimonial-paragraph'>
-                        <span>"</span>Having worked for and with Avant projects in various roles since 1990 , I have been very satisfied with their focus 
-                        and drive applicable to the support and cooperation I had in all my roles in the company. They deal with any issues 
-                        that need additional attention and keep their customers up to date with progress. I recommend their reliability... <span>"</span>
+                        <span>"</span>Avant Projects has been a customer of Pronto Ready Mix since 2002 and we have maintained a good business relationship 
+                        during this time. We find their employees knowledgeable in all aspects of the projects being carried out. If needed, they have full 
+                        access to our resident Engineer, Johan Ras for any assistance required... <span>"</span>
                 </p>
                 <a href="">
                     <button className='button1'>Open Original Testimonial</button>
