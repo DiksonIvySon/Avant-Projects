ca4e4406dcab73e10a1da258aa93edb6d7a38cdf
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-11-26 13:04:36 +0300

    Worked on the Team Member Card overlay.

--- AvantProjects/src/Components/TeamMember.jsx
+++ AvantProjects/src/Components/TeamMember.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TeamMember.css';
 import tra_person from '../images/tra_person.png';
 
@@ -6,15 +6,32 @@ import tra_person from '../images/tra_person.png';
 
 
 function TeamMember() {
+  const [overlay, setOverlay] = useState("team-member-image-overlay-hide");
+
+  const handleMouseOver = () => {
+    setOverlay("team-member-image-overlay");
+  }
+
+  const handleMouseLeave = () => {
+    setOverlay("team-member-image-overlay-hide");
+  }
+
   return (
     <div className='team-member'>
-        <div class="card">
+        <div className="card">
             <div className='team-member-image'>
                 <img src={tra_person} alt="Team member image" />
+                <div className={overlay} onMouseMove={handleMouseOver} onMouseLeave={handleMouseLeave}>
+                  <div className='team-member-overlay-text'>
+                    <p>
+                      15 years <br />of construction <br />industry experience
+                    </p>
+                  </div>
+                </div>
             </div>
-            <div class="container">
+            <div className="container">
                 <h2>Andre Van Tonder</h2>
-                <p class="title">CEO &amp; Founder</p>
+                <p className="title">CEO &amp; Founder</p>
                 <p>Some text that describes me.</p>
             </div>
         </div>
