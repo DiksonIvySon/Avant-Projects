6119df70973d83b57b1ff6caaf90284718f3f07f
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-12-01 13:23:51 +0300

    Fixed CallToAction component overlay error

--- AvantProjects/src/Components/CallToAction.jsx
+++ AvantProjects/src/Components/CallToAction.jsx
@@ -5,14 +5,14 @@ import callToAct_background from '../images/callToAct_background.jpg';
 function CallToAction() {
   return (
     <div className='CallToAction' style={{backgroundImage: `url(${callToAct_background})`}} >
-        <div className='overlay'></div>
-        <div className='callToAct-content'>
+        <div className='callToAction-content'>
             <h1>Ready To Start A New Project With Us?</h1>
             <p>We strive to build lasting friendships and business relations based on a quality service.</p>
             <a href="">
                 <button className='button1'>Contact Us</button>
             </a>
         </div>
+        <div className='overlay'></div>
     </div>
   )
 }
