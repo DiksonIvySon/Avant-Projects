db06888362b3d15aff1e14fc331015685062a922
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-12-02 20:10:18 +0300

    Worked on the structure of the Services list section.

--- AvantProjects/src/Components/ServicesList.jsx
+++ AvantProjects/src/Components/ServicesList.jsx
@@ -75,36 +75,328 @@ function ServicesList() {
         </div>
         <div className='services-content-container'>
             <div className='service-content' id={industrial}>
-                <h1>Industrial Construction</h1>
-                <p>
+                <h1 className='service-content-heading'>Industrial Construction</h1>
+                <p className='service-content-paragraph'>
                     With more than 35 years of construction experience, we also provide residential, 
                     commercial, and infrastructure construction services across the Gauteng Province. 
                     You are in capable hands, with Avant Projects.
                 </p>
+                <div className='servicesList'>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>1</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>2</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>3</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>4</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>5</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>6</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>7</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>8</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>9</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number two-number-listItem'>10</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                </div>
             </div>
             <div className='service-content' id={residential}>
-                <h1>Residential Construction</h1>
-                <p>
+                <h1 className='service-content-heading'>Residential Construction</h1>
+                <p className='service-content-paragraph'>
                     With more than 35 years of construction experience, we also provide residential, 
                     commercial, and infrastructure construction services across the Gauteng Province. 
                     You are in capable hands, with Avant Projects.
                 </p>
+                <div className='servicesList'>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>1</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>2</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>3</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>4</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>5</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>6</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>7</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>8</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>9</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number two-number-listItem'>10</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                </div>
             </div>
             <div className='service-content' id={infrastructural}>
-                <h1>Infrastructural Construction</h1>
-                <p>
+                <h1 className='service-content-heading'>Infrastructural Construction</h1>
+                <p className='service-content-paragraph'>
                     With more than 35 years of construction experience, we also provide residential, 
                     commercial, and infrastructure construction services across the Gauteng Province. 
                     You are in capable hands, with Avant Projects.
                 </p>
+                <div className='servicesList'>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>1</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>2</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>3</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>4</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>5</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>6</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>7</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>8</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>9</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number two-number-listItem'>10</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                </div>
             </div>
             <div className='service-content' id={commercial}>
-                <h1>Commercial Construction</h1>
-                <p>
+                <h1 className='service-content-heading'>Commercial Construction</h1>
+                <p className='service-content-paragraph'>
                     With more than 35 years of construction experience, we also provide residential, 
                     commercial, and infrastructure construction services across the Gauteng Province. 
                     You are in capable hands, with Avant Projects.
                 </p>
+                <div className='servicesList'>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>1</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>2</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>3</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>4</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>5</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>6</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>7</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>8</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number'>9</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                    <div className='listItem'>
+                        <h2 className='listItem-number two-number-listItem'>10</h2> 
+                        <div className='listItem-info'>
+                            <h2>Construction</h2>
+                            <p>residential, commercial, and infrastructure construction services</p>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+            <div className='services-callToAction'>
+                <p>Should the service you require not be listed above, please feel free to contact us.</p>
+                <button className='button2'>Contact Us</button>
             </div>
         </div>
     </div>
