b80027739101330b20be74dddf5ade5b84e2695c
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-12-02 07:20:01 +0300

    Worked on the services section in the services page.

--- AvantProjects/src/pages/Services.jsx
+++ AvantProjects/src/pages/Services.jsx
@@ -42,6 +42,42 @@ function Services() {
                     </div>
                 </div>
             </div>
+            <div>
+                <div className='sectionHeading servicesHeading'>
+                    <h1>Avant Projects</h1>
+                    <p>Services </p>
+                </div>
+                <div className='services-page-services-paragraph'>
+                    <p>
+                        With more than 35 years of construction experience, we also provide residential, 
+                        commercial, and infrastructure construction services across the Gauteng Province. 
+                        You are in capable hands, with Avant Projects.
+                    </p>
+                </div>
+                <div className='services-page-servicesContainer'>
+                    <div className='service-page-service'>
+                        <img src={working} alt="Service Image" />
+                        <div className='service-type'>
+                            <p>Residential</p>
+                            <p>Construction</p>
+                        </div>
+                    </div>
+                    <div className='service-page-service'>
+                        <img src={working} alt="Service Image" />
+                        <div className='service-type'>
+                            <p>Infrastructural</p>
+                            <p>Construction</p>
+                        </div>
+                    </div>
+                    <div className='service-page-service'>
+                        <img src={working} alt="Service Image" />
+                        <div className='service-type'>
+                            <p>Commercial</p>
+                            <p>Construction</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div className='servicesBody'> 
         <ServicesSection />
