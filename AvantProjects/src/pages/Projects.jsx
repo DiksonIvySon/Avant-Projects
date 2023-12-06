04a93bd6200e1dac5651d114e7b76edefeb98e60
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-12-06 09:06:14 +0300

    Added all the correct project picyures.

--- AvantProjects/src/pages/Projects.jsx
+++ AvantProjects/src/pages/Projects.jsx
@@ -10,6 +10,47 @@ import FooterSection from '../Components/FooterSection';
 
 import hard_hat from '../images/hard_hat.png';
 import working from '../images/working.jpeg';
+//NATURES GARDEN PICTURES
+import picture57 from '../images_projects/Picture57.jpg';
+import picture58 from '../images_projects/Picture58.jpg';
+import picture60 from '../images_projects/Picture60.jpg';
+//TATA PICTURES
+import picture0 from '../images_projects/Picture0.jpg';
+import picture1 from '../images_projects/Picture1.jpg';
+import picture3 from '../images_projects/Picture3.png';
+//TIGER BRANDS PICTURES
+import picture19 from '../images_projects/Picture19.jpg';
+import picture20 from '../images_projects/Picture20.jpg';
+import picture18 from '../images_projects/Picture18.jpg';
+//MARLEY PICTURES (BREAK PAVING)
+import picture21 from '../images_projects/Picture21.jpg';
+import picture22 from '../images_projects/Picture22.jpg';
+import picture23 from '../images_projects/Picture23.jpg';
+//GEA PICTURES
+import picture40 from '../images_projects/Picture40.jpg';
+import picture39 from '../images_projects/Picture39.jpg';
+import picture41 from '../images_projects/Picture41.jpg';
+//SEEFFAM PICTURES
+import picture51 from '../images_projects/Picture51.jpg';
+import picture52 from '../images_projects/Picture52.jpg';
+import picture53 from '../images_projects/Picture53.jpg';
+//SEEFFAM PICTURES 
+import picture7 from '../images_projects/Picture7.jpg';
+import picture8 from '../images_projects/Picture8.jpg';
+import picture9 from '../images_projects/Picture9.jpg';
+//MARLEY PICTURES
+import picture26 from '../images_projects/Picture26.jpg';
+import picture27 from '../images_projects/Picture27.jpg';
+import picture28 from '../images_projects/Picture28.jpg';
+//MCCAIN PICTURES
+import picture48 from '../images_projects/Picture48.jpg';
+import picture49 from '../images_projects/Picture49.jpg';
+import picture50 from '../images_projects/Picture50.jpg';
+//GEA PICTURES
+import picture42 from '../images_projects/Picture42.jpg';
+import picture43 from '../images_projects/Picture43.jpg';
+import picture44 from '../images_projects/Picture44.jpg';
+
 
 function Projects() {
 
@@ -30,81 +71,90 @@ function Projects() {
                 <h1>Nature Garden</h1>
                 <p>Glass enclosure for divided office spaces</p>
                 <div class="scroll-container">
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
+                    <img src={picture57} alt="Project Picture" />
+                    <img src={picture58} alt="Project Picture" />
+                    <img src={picture60} alt="Project Picture" />
                 </div>
             </div> 
             <div className='projects-page-project'>
                 <h1>TATA - ISANDO</h1>
                 <p>Polyurethane floor coating</p>
                 <div class="scroll-container">
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
+                    <img src={picture0} alt="Project Picture" />
+                    <img src={picture1} alt="Project Picture" />
+                    <img src={picture3} alt="Project Picture" />
                 </div>
             </div> 
             <div className='projects-page-project'>
                 <h1>Tiger Brands</h1>
                 <p>Built the perimeter wall</p>
                 <div class="scroll-container">
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
+                    <img src={picture19} alt="Project Picture" />
+                    <img src={picture20} alt="Project Picture" />
+                    <img src={picture18} alt="Project Picture" />
                 </div>
             </div> 
             <div className='projects-page-project'>
                 <h1>Marley</h1>
-                <p>Break Paving</p>
+                <p>Brick Paving</p>
                 <div class="scroll-container">
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
+                    <img src={picture21} alt="Project Picture" />
+                    <img src={picture22} alt="Project Picture" />
+                    <img src={picture23} alt="Project Picture" />
                 </div>
             </div> 
             <div className='projects-page-project'>
                 <h1>GEA</h1>
                 <p>Complete new plant (concrete, structural steel, insulation and mezzanine)</p>
                 <div class="scroll-container">
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
+                    <img src={picture40} alt="Project Picture" />
+                    <img src={picture39} alt="Project Picture" />
+                    <img src={picture41} alt="Project Picture" />
                 </div>
             </div> 
             <div className='projects-page-project'>
                 <h1>Seeffam</h1>
                 <p>Press machine foundation and bund walls</p>
                 <div class="scroll-container">
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
+                    <img src={picture51} alt="Project Picture" />
+                    <img src={picture52} alt="Project Picture" />
+                    <img src={picture53} alt="Project Picture" />
                 </div>
             </div> 
             <div className='projects-page-project'>
                 <h1>Seeffam</h1>
-                <p>Torch on waterproofing</p>
+                <p>Waterproofing</p>
                 <div class="scroll-container">
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
+                    <img src={picture7} alt="Project Picture" />
+                    <img src={picture8} alt="Project Picture" />
+                    <img src={picture9} alt="Project Picture" />
                 </div>
             </div>
             <div className='projects-page-project'>
                 <h1>Marly</h1>
                 <p>Power floated concrete</p>
                 <div class="scroll-container">
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
+                    <img src={picture26} alt="Project Picture" />
+                    <img src={picture27} alt="Project Picture" />
+                    <img src={picture28} alt="Project Picture" />
                 </div>
             </div>
             <div className='projects-page-project'>
                 <h1>McCain</h1>
                 <p>Plints cast floor repair and steel work</p>
                 <div class="scroll-container">
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
-                    <img src={working} alt="" />
+                    <img src={picture48} alt="Project Picture" />
+                    <img src={picture49} alt="Project Picture" />
+                    <img src={picture50} alt="Project Picture" />
+                </div>
+            </div>
+            <div className='projects-page-project'>
+                <h1>GEA</h1>
+                <p>Mezzanine floor cast and hand rails</p>
+                <div class="scroll-container">
+                    <img src={picture42} alt="Project Picture" />
+                    <img src={picture43} alt="Project Picture" />
+                    <img src={picture44} alt="Project Picture" />
                 </div>
             </div>
         </div>
