76342b552818115f004501d17ee575e594e437f3
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-12-07 09:52:13 +0300

    Changed the About page structure and added more content.

--- AvantProjects/src/pages/About.jsx
+++ AvantProjects/src/pages/About.jsx
@@ -34,6 +34,41 @@ function About() {
                 result, we have established a solid reputation as leaders in the field. As your dependable and 
                 trustworthy building partner, we are prepared to realize your vision.
             </p>
+            <div className='about-page-questions'>
+                <div className='about-question'>
+                    <h1><span>WHO</span> ARE <br /> WE?</h1>
+                    <p>
+                        Avant Projects is a Gauteng based construction company. Run by Cecilia and Andre Van Tonder.
+                        <br />
+                        Cecilia and Andre Van Tonder founded Avant Projects in 1990 with passion in the construction industry.
+                        Since then, Avant Projects has collaborated on numerous initiatives with numerous significant businesses, 
+                        including TATA and Tiger Brands.
+                    </p>
+                </div>
+                <div className='about-question'>
+                    <h1><span>WHAT</span> WE <br /> GUARANTEE?</h1>
+                    <p>
+                        From the beginning to the conclusion, we want every project to be properly handled and carried out. to keep you 
+                        up to date, assured, and content at every stage of the undertaking.
+                        <br />
+                        By keeping everyone engaged producing work of the highest caliber, we want to achieve this. For every procedure, 
+                        we create and manage reasonable budgets. We collaborate with a network of reliable, inspired vendors and experts 
+                        that we can rely on. Honest counsel, transparent communication, and prudent project management are things we take 
+                        great delight in.
+                    </p>
+                </div>
+                <div className='about-question'>
+                    <h1><span>WHY</span> DO <br /> WE DO THIS?</h1>
+                    <p>
+                        text here text here text here text here text here text here text here text here text here text here text here 
+                        text here text here text here text here text here text here
+                        <br />
+                        text here text here text here text here text here text here text here text here text here text here text here 
+                        text here text here text here text here text here text here text here text here text here text here text here 
+                        text here text here text here text here text here text here
+                    </p>
+                </div>
+            </div>
             <div className='about-page-pillars'>
                 <div className='foundational-pillars'>
                     <h2>Our Foundational Pillars</h2>
@@ -70,37 +105,30 @@ function About() {
         <CallToAction />
         <div className='section'>
             <div className='about-page-choose'>
-                <div className='about-page-image'>
-                    <div className='image img-background-shadow'>  
-                    </div>
-                    <div className='image'>
-                        <img className='about-main-photo' src={working} alt="about image" />
-                    </div>
-                </div>
                 <div className='about-page-info'>
                     <h2>Choose Avant Projects For:</h2>
-                    <ol>
-                        <li>
-                            <b>High-quality Workmanship:</b> The members of our 
+                    <div>
+                        <p>
+                            <b>High-quality Workmanship:</b> <br /> The members of our 
                             team are proficient experts committed to providing 
                             each project with work of an outstanding quality.
-                        </li>
-                        <li>
-                            <b>Timely Completion:</b> We recognize the value of 
+                        </p>
+                        <p>
+                            <b>Timely Completion:</b> <br /> We recognize the value of 
                             completing tasks by the deadline. To guarantee that 
                             your job is finished on schedule, we work promptly.
-                        </li>
-                        <li>
-                            <b>Attention to Detail:</b> We take great care to 
+                        </p>
+                        <p>
+                            <b>Attention to Detail:</b> <br /> We take great care to 
                             notice even the smallest details, so that your job 
                             is completed perfectly.
-                        </li>
-                        <li>
-                            <b>Customer Satisfaction:</b> Our priority is your 
+                        </p>
+                        <p>
+                            <b>Customer Satisfaction:</b> <br /> Our priority is your 
                             satisfaction. We work closely with our clients, 
                             keeping them informed throughout the construction process.
-                        </li>
-                    </ol>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
