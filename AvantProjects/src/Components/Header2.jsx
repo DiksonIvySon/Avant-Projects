714da524bf6e7b72079f0ff7d80059253c76de76
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-12-28 18:00:15 +0300

    Fixed the react routing error.

--- AvantProjects/src/Components/Header2.jsx
+++ AvantProjects/src/Components/Header2.jsx
@@ -29,10 +29,10 @@ function Header2() {
         <div id="main-nav" className={verticalMenu} >
             <ul>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="about">About</Link></li>
-                <li><Link to="services">Services</Link></li>
-                <li><Link to="projects">Projects</Link></li>
-                <li><Link to="contact">Contact</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/services">Services</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
           </ul>
             <div className='get_a_quote-button'>
                 <a href="">
