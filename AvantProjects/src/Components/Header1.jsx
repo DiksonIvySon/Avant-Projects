9c00fe7c09648ced0972aba5d8aca644313ad5ca
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2024-01-13 21:09:26 +0300

    Changed the structure of header1 component.

--- AvantProjects/src/Components/Header1.jsx
+++ AvantProjects/src/Components/Header1.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './Header1.css'
+import { FaFacebookSquare, FaLinkedin } from 'react-icons/fa';
 
 function Header1() {
   return (
     <div className='header1'>
         <div className='operating-times'>
             <p>Mon-Fri: 8.00AM - 5.00pm</p>
+        </div>
+        <div className='header1-socials'>
+            <hr />
+            <a href="">
+                <FaFacebookSquare />
+            </a>
+            <hr />
+            <a href="">
+                <FaLinkedin />
+            </a>
+            <hr />
         </div>
     </div>
   )
